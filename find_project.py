b9288e0dda86ad3651c78479684126435e81f98b
Rafael Silva <107515625+rafaeldpsilva@users.noreply.github.com>
2026-02-26 14:48:56 +0000

    Commit from thinkpad on 2/26/2026, 2:48:55 PM

--- find_project.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-url = "http://192.168.2.91:3456/api/v1"
-token = "tk_a3f9c49bdc19b5fc164ce95e4df6f6df8374f568"
-headers = {"Authorization": f"Bearer {token}"}
-r = requests.get(f"{url}/projects", headers=headers, timeout=10)
-for p in r.json():
-    print(f"ID: {p['id']}, Title: {p['title']}")
